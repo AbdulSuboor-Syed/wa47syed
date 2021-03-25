ba41279c8c70864706bce5cef495394e34980f0b
Syed, Abdul Suboor <77645775+AbdulSuboor-Syed@users.noreply.github.com>
2021-03-24 19:30:42 -0500

    bonus

--- routes/computation.js
+++ routes/computation.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res)=>{
+  let randomNumber=Math.floor(Math.random()*100)+1
+  let data=req.query.x
+  console.log(data)
+  res.render('computation',{
+    randomSolution:`sqrt of ${randomNumber} is ${Math.sqrt(randomNumber)}`,
+    querySolution:`sqrt to ${data} is ${Math.sqrt(data)}`
+  })
 });
 
 module.exports = router;
